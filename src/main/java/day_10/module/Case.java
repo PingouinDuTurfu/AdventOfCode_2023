e30135e01c7445ab69682ae88538dff440701e8f
ollivier remy <remy.olliver@gmail.com>
2023-12-11 05:57:04 +0100

    Add day 10

--- src/main/java/day_10/module/Case.java
+++ src/main/java/day_10/module/Case.java
@@ -1,31 +1,3 @@
 package day_10.module;
 
-public class Case {
-    private final int x;
-    private final int y;
-    private final int c;
-    Character p;
-
-    public Case(int x, int y, Character p, int c) {
-        this.x = x;
-        this.y = y;
-        this.c = c;
-        this.p = p;
-    }
-
-    public int x() {
-        return x;
-    }
-
-    public int y() {
-        return y;
-    }
-
-    public int c() {
-        return c;
-    }
-
-    public Character p() {
-        return p;
-    }
-}
+public record Case(int x, int y, Character p, int c) {}
